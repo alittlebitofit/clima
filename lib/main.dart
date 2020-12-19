b19b5e466547515c527a368d7a21f499ab112867
Your Name <you@example.com>
2020-12-19 11:07:26 +0530

    commit before upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,23 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:clima/screens/loading_screen.dart';
 
-void main() => runApp(MyApp());
+
+void main() {
+  HttpOverrides.global = new MyHttpOverrides();
+  runApp(MyApp());
+}
+
+class MyHttpOverrides extends HttpOverrides{
+  @override
+  HttpClient createHttpClient(SecurityContext context){
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
+  }
+}
+
 
 class MyApp extends StatelessWidget {
 
